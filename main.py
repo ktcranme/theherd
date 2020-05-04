4430b58332f7dc610da26117f4b54f701c5cba02
Protactini <56206430+Protactini@users.noreply.github.com>
2020-05-03 22:03:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,54 @@
 from flask import Flask, render_template
+from gql import gql, Client
+from gql.transport.requests import RequestsHTTPTransport
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+	authToken = 'b840d07ebf9af93837d3b53fd1ff1aec'
+
+	sample_transport=RequestsHTTPTransport(
+    		url='https://api.smash.gg/gql/alpha',
+    		use_json=True,
+    		headers={
+        		"Content-type": "application/json",
+        		'Authorization': "Bearer {}".format(authToken)
+    			}
+		)
+
+	client = Client(
+    		retries=3,
+    		transport=sample_transport,
+    		fetch_schema_from_transport=True,
+	)
+
+	query1 = gql('''
+  		query Sets {  
+  		player(id: 1000) {
+    		id
+    		sets(perPage: 5, page: 10) {
+      		nodes {
+        		id
+        		displayScore
+        		event {
+          			id
+          			name
+          			tournament {
+            					id
+            					name
+          			}
+        		}
+      		}
+    		}
+  		}
+  		}
+	''')
+
+	getSeedsResult = client.execute(query1)
+
+	#resData = json.loads(getSeedsResult)
+	print(getSeedsResult)
 	return 'This is the homepage'
 
 
