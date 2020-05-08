b7266d51623019a5d45ced06f96b9f527adfdd0d
moulaskar <47702408+moulaskar@users.noreply.github.com>
2020-05-08 01:22:11 -0700

    Add files via upload
    Alex's changes

--- routes/playerscout.py
+++ routes/playerscout.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 import time
+import json
 playerScout_bp = Blueprint('player_scout_api', __name__, url_prefix='/scout-player')
 
 authToken = 'b840d07ebf9af93837d3b53fd1ff1aec'
@@ -11,15 +12,60 @@ def testing():
     player_id = request.args.get('player-id')
     print("inside the API")
     results = perform_scout_api_call(player_id)
+    nametag = results["player"]["gamerTag"]
 
-    data = {
-        "gamerTag": results['something idk get the gamerTag here'],
-        "sets": []
-    }
+    data = {"gamerTag":results["player"]["gamerTag"],"sets":[]}
+           
+
+    for tour in results['player']['recentSets']:
+      tourinfo = tour["displayScore"]
+      indexforin = tourinfo.find('-')
+      opponent = tourinfo[indexforin+1:]
+      playerwithteam = tourinfo[:indexforin-1]
+      ind = opponent.find(nametag)
+      if ind != -1:
+        opponent = tourinfo[:indexforin-1]
+        playerwithteam = tourinfo[indexforin+1:]
+
+      #for participents in tour['slots']:
+      prot_id1 = json.dumps(tour['slots'][1]['entrant'])
+      prot_id2 = json.dumps(tour['slots'][1]['entrant'])
+      player_id1 = str(player_id)
+      ind = prot_id1.find(player_id1)
+      if ind != -1:
+        ind = prot_id1.find('"id":')
+        opponent_id = prot_id1[ind+5:ind+11]
+      else:
+        ind = prot_id2.find('"id":')
+        opponent_id = prot_id2[ind+5:ind+11]
+       
+      try:
+        opponent_id = int(opponent_id)
+      except:
+        continue
+
+      score1 = playerwithteam[-1]
+      score2 = opponent[-1]
+      if score1 > score2:
+        winner = playerwithteam[:-2]
+      else:
+        winner = opponent[:-2]
+
+      add_tour = {"opponent": opponent[:-2],
+				      "opponent_id": opponent_id,
+				      "winner": winner,
+				      "opponentGamesWon": score2,
+				      "playerGamesWon": score1,
+				      "date": tour['completedAt'],
+				      "tournament": tour["event"]["tournament"]["name"],
+				      "event": tour["event"]["name"]}
+      data["sets"].append(add_tour)
+     
 
     #here loop through all the sets inside the results and append them to
     #the 'sets' attribute inside the data object. Try to follow the format
     #I have specified in playerscout.js
+
 
 
     return jsonify(data), 200
@@ -40,19 +86,42 @@ def perform_scout_api_call(player_id):
             fetch_schema_from_transport=True,
     )
 
-    """
+    
 
     #write the query here
     query1 = gql('''
-        
+        query Playerscout($playerID: ID!) {
+                player(id: $playerID) {
+                  gamerTag
+                  recentSets {
+                      id
+                      displayScore
+                      completedAt
+                      slots{
+                        entrant{
+                            participants{
+                              connectedAccounts
+                              player{
+                                id
+                                gamerTag
+                              }
+                            }
+                          }
+                      }
+                      event {
+                        name
+                        tournament {
+                          name
+                        }
+                      }
+                    
+                  }
+                }
+              }
     ''')
     #write the variables here
     variables = {
-        
+        "playerID":player_id
     }
     results = client.execute(query1, variables)
     return results
-
-    """
-
-    return {'something idk get the gamerTag here': "cranberry"}
